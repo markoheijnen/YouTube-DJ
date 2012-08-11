3310c9e46b3493555515b409c24777d195c3316f
Marko Heijnen <info@markoheijnen.com>
2012-08-11 10:25:30 +0200

    Modify search url to only show embed videos

--- inc/search.php
+++ inc/search.php
@@ -31,7 +31,8 @@ class Youtubedj_Search {
 		$max_results = 10;
 		$start_index = absint( $_REQUEST['offset'] );
 
-		$url = 'https://gdata.youtube.com/feeds/api/videos?q=' . $search . '&max-results=' . $max_results . '&start-index=' . $start_index . '&v=2&alt=jsonc';
+		//format=5 = embed only and format=1,6 is mobile only
+		$url = 'https://gdata.youtube.com/feeds/api/videos?q=' . $search . '&max-results=' . $max_results . '&start-index=' . $start_index . '&format=1,5,6&v=2&alt=jsonc';
 
 		$response = wp_remote_get( $url );
 		$data     = json_decode( wp_remote_retrieve_body( $response ) );
