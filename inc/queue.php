93fc2db66267ee394a9d40a421309a0724fca069
Marko Heijnen <info@markoheijnen.com>
2013-12-26 19:59:18 +0100

    Show queue list. See #3

--- inc/queue.php
+++ inc/queue.php
@@ -6,11 +6,16 @@ class Youtubedj_Queue {
 		
 	}
 
-	public function html( $id, $title, $decks, $queuelist ) {
+	public function html( $id, $title, $decks, $queuelist = array() ) {
 		$html  = '<div id="' . $id . '" class="queue gear" decks="' . implode( ',', $decks ) . '">';
 		$html .= '<h2>' . $title . '</h2>';
 
 		$html .= '<ul class="queuelist">';
+
+		foreach( $queuelist as $item ) {
+			$html .= '<li songid="' . $item['id'] . '" class="song"><h5>' . $item['title'] . '</h5></li>';
+		}
+
 		$html .= '</ul>';
 
 		$html .= '</div>';
