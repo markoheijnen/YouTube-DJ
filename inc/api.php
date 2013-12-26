447fd592b733bf4247567f6a067fbd97328a3384
Marko Heijnen <info@markoheijnen.com>
2013-12-26 19:20:49 +0100

    Start index need to be 1 and not 0. Also check in normalizer if the request didn't had an error. See #3

--- inc/api.php
+++ inc/api.php
@@ -3,7 +3,7 @@
 class Youtubedj_API {
 	//format=5 = embed only and format=1,6 is mobile only
 
-	public static function user_playlist( $user, $max_results = 25, $start_index = 0 ) {
+	public static function user_playlist( $user, $max_results = 25, $start_index = 1 ) {
 		$user = sanitize_text_field( $user );
 		$url  = 'https://gdata.youtube.com/feeds/api/users/' . $user . '/uploads?max-results=' . $max_results . '&start-index=' . $start_index . '&format=1,5,6&v=2&alt=jsonc';
 
@@ -13,7 +13,7 @@ class Youtubedj_API {
 		return self::normalize( $data );
 	}
 
-	public static function search( $search, $max_results = 25, $start_index = 0 ) {
+	public static function search( $search, $max_results = 25, $start_index = 1 ) {
 		$search = sanitize_text_field( $search );
 		$url    = 'https://gdata.youtube.com/feeds/api/videos?q=' . $search . '&max-results=' . $max_results . '&start-index=' . $start_index . '&format=1,5,6&v=2&alt=jsonc';
 
@@ -27,7 +27,7 @@ class Youtubedj_API {
 	private static function normalize( $data ) {
 		$response = array( 'total' => 0, 'max_results' => $max_results, 'start_index' => $start_index, 'songs' => array() );
 
-		if( ! empty( $data ) ) {
+		if( ! empty( $data ) && ! isset( $data->error ) ) {
 			$response['total']       = $data->data->totalItems;
 			$response['start_index'] = $data->data->startIndex;
 			$items                   = $data->data->items;
